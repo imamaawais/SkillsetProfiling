f36f1fd8f3017d0abd0dbe5372807773a3edc13c
MYasirJamal <84134206+MYasirJamal@users.noreply.github.com>
2024-02-18 22:44:58 +0500

    All files primary/foreign keys done

--- src/main/java/com/example/SkillsetProfiling/Key/Badge_Assignment_Key.java
+++ src/main/java/com/example/SkillsetProfiling/Key/Badge_Assignment_Key.java
@@ -1,7 +1,16 @@
 package com.example.SkillsetProfiling.Key;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Badge_Assignment_Key implements Serializable {
     private Integer StudentID;
     private Integer SkillID;
--- src/main/java/com/example/SkillsetProfiling/Key/Student_Experience_Key.java
+++ src/main/java/com/example/SkillsetProfiling/Key/Student_Experience_Key.java
@@ -1,7 +1,16 @@
 package com.example.SkillsetProfiling.Key;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student_Experience_Key implements Serializable {
     private Integer StudentID;
     private Integer ExperienceID;
