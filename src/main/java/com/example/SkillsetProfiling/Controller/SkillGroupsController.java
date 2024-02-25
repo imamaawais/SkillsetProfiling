53f11ff73a0a5997031c44268559cad867036edd
Imama <syedaimamaawais@gmail.com>
2024-02-25 17:43:50 +0500

    Fixed Skill Groups Controller

--- src/main/java/com/example/SkillsetProfiling/Controller/SkillGroupsController.java
+++ src/main/java/com/example/SkillsetProfiling/Controller/SkillGroupsController.java
@@ -1,13 +1,16 @@
 package com.example.SkillsetProfiling.Controller;
 
 import com.example.SkillsetProfiling.Dto.Auth_DTO;
+import com.example.SkillsetProfiling.Dto.Role_DTO;
 import com.example.SkillsetProfiling.Dto.Skill_Groups_DTO;
+import com.example.SkillsetProfiling.Exception.SkillGroupNotFoundException;
 import com.example.SkillsetProfiling.Service.Implementation.Skill_Groups_Service;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.relation.RoleNotFoundException;
 import java.util.List;
 
 @AllArgsConstructor
@@ -19,7 +22,7 @@ public class SkillGroupsController {
     private Skill_Groups_Service service;
 
     @PostMapping
-    public ResponseEntity<Skill_Groups_DTO> addSkillGroup(@RequestBody Skill_Groups_DTO skillGroupsDTO){
+    public ResponseEntity<Skill_Groups_DTO> addSkillGroup(@RequestBody Skill_Groups_DTO skillGroupsDTO) {
         Skill_Groups_DTO savedSkillGroup = service.addSkillGroup(skillGroupsDTO);
         return new ResponseEntity<>(savedSkillGroup, HttpStatus.CREATED);
     }
@@ -33,5 +36,36 @@ public class SkillGroupsController {
         }
 
         return new ResponseEntity<>(skillGroups, HttpStatus.OK);
+    }
+
+    @GetMapping("/id/{SkillGroupId}")
+    public ResponseEntity<Skill_Groups_DTO> getSkillGroupById(@PathVariable int SkillGroupId) throws SkillGroupNotFoundException {
+        Skill_Groups_DTO skillGroupsDTO = service.getSkillGroupById(SkillGroupId);
+
+        return new ResponseEntity<>(skillGroupsDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{SkillGroupId}")
+    public ResponseEntity<Skill_Groups_DTO> updateSkillGroup(@PathVariable Integer SkillGroupId, @RequestBody Skill_Groups_DTO updatedSkillGroupsDTO) {
+        try {
+            Skill_Groups_DTO updatedSkillGroup = service.updateSkillGroup(SkillGroupId, updatedSkillGroupsDTO);
+            return new ResponseEntity<>(updatedSkillGroup, HttpStatus.OK);
+        } catch (SkillGroupNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{SkillGroupId}")
+    public ResponseEntity<Void> deleteSkillGroup(@PathVariable Integer SkillGroupId) {
+        try {
+            boolean deleted = service.deleteSkillGroup(SkillGroupId);
+            if (deleted) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (SkillGroupNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/example/SkillsetProfiling/Service/Interface/ISkill_Groups_Service.java
+++ src/main/java/com/example/SkillsetProfiling/Service/Interface/ISkill_Groups_Service.java
@@ -2,6 +2,7 @@ package com.example.SkillsetProfiling.Service.Interface;
 
 import com.example.SkillsetProfiling.Dto.Auth_DTO;
 import com.example.SkillsetProfiling.Dto.Skill_Groups_DTO;
+import com.example.SkillsetProfiling.Exception.SkillGroupNotFoundException;
 import com.example.SkillsetProfiling.Exception.UserNotFoundException;
 
 import java.util.List;
@@ -9,9 +10,9 @@ import java.util.List;
 public interface ISkill_Groups_Service {
 
     Skill_Groups_DTO addSkillGroup(Skill_Groups_DTO skillGroupsDTO);
-    Skill_Groups_DTO getSkillGroupById(Integer SkillGroupID) throws UserNotFoundException;
+    Skill_Groups_DTO getSkillGroupById(Integer SkillGroupID) throws SkillGroupNotFoundException;
     List<Skill_Groups_DTO> getAllSkillGroups();
-    Skill_Groups_DTO updateSkillGroup(String SkillGroupID, Skill_Groups_DTO updatedSkillGroupsDTO);
+    Skill_Groups_DTO updateSkillGroup(Integer SkillGroupID, Skill_Groups_DTO updatedSkillGroupsDTO);
 
     boolean deleteSkillGroup(Integer SkillGroupID);
 }
