6b41cdcb29eb58afbaa3d300f6d16f342f845694
Imama <syedaimamaawais@gmail.com>
2024-02-25 17:44:43 +0500

    Fixed Skill Groups Controller

--- src/main/java/com/example/SkillsetProfiling/Service/Implementation/Skill_Groups_Service.java
+++ src/main/java/com/example/SkillsetProfiling/Service/Implementation/Skill_Groups_Service.java
@@ -5,14 +5,18 @@ import com.example.SkillsetProfiling.Dto.Skill_Groups_DTO;
 import com.example.SkillsetProfiling.Entity.Role;
 import com.example.SkillsetProfiling.Entity.Skill_Groups;
 import com.example.SkillsetProfiling.Exception.DuplicateSkillGroupException;
+import com.example.SkillsetProfiling.Exception.SkillGroupNotFoundException;
 import com.example.SkillsetProfiling.Exception.UserNotFoundException;
 import com.example.SkillsetProfiling.Repository.Skill_Groups_Repo;
 import com.example.SkillsetProfiling.Service.Interface.ISkill_Groups_Service;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.RoleNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,8 +44,14 @@ public class Skill_Groups_Service implements ISkill_Groups_Service {
     }
 
     @Override
-    public Skill_Groups_DTO getSkillGroupById(Integer SkillGroupID) throws UserNotFoundException {
-        return null;
+    public Skill_Groups_DTO getSkillGroupById(Integer SkillGroupID) throws SkillGroupNotFoundException {
+        Optional<Skill_Groups> skillGroupsOptional = skillGroupsRepo.findById(SkillGroupID);
+
+        if (skillGroupsOptional.isPresent()) {
+            return mapper.map(skillGroupsOptional.get(), Skill_Groups_DTO.class);
+        } else {
+            throw new SkillGroupNotFoundException("Skill Group not found with id: " + SkillGroupID);
+        }
     }
 
     @Override
@@ -55,12 +65,33 @@ public class Skill_Groups_Service implements ISkill_Groups_Service {
     }
 
     @Override
-    public Skill_Groups_DTO updateSkillGroup(String SkillGroupID, Skill_Groups_DTO updatedSkillGroupsDTO) {
-        return null;
+    public Skill_Groups_DTO updateSkillGroup(Integer SkillGroupID, Skill_Groups_DTO updatedSkillGroupsDTO) {
+        Optional<Skill_Groups> skillGroupsOptional = skillGroupsRepo.findById(SkillGroupID);
+
+        if (skillGroupsOptional.isPresent()) {
+            Skill_Groups existingSkillGroup = skillGroupsOptional.get();
+
+            existingSkillGroup.setGroupName(updatedSkillGroupsDTO.getGroupName());
+
+            Skill_Groups updatedSkillGroup = skillGroupsRepo.save(existingSkillGroup);
+
+            return mapper.map(updatedSkillGroup, Skill_Groups_DTO.class);
+        } else {
+            throw new SkillGroupNotFoundException("Skill Group not found with ID: " + SkillGroupID);
+        }
     }
 
     @Override
+    @Transactional
     public boolean deleteSkillGroup(Integer SkillGroupID) {
-        return false;
+        Optional<Skill_Groups> skillGroupsOptional = skillGroupsRepo.findById(SkillGroupID);
+
+        if (skillGroupsOptional.isPresent()) {
+            Skill_Groups skillGroupToDelete = skillGroupsOptional.get();
+            skillGroupsRepo.delete(skillGroupToDelete);
+            return true; // Deletion successful
+        } else {
+            throw new SkillGroupNotFoundException("Skill Group not found with ID: " + SkillGroupID);
+        }
     }
 }
