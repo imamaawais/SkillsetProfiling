9f7e8aa3cb88ded2e82439170e53f491001373e0
MYasirJamal <84134206+MYasirJamal@users.noreply.github.com>
2024-03-11 18:21:00 +0500

    commented options cz error

--- src/main/java/com/example/SkillsetProfiling/Controller/QuestionOptionsController.java
+++ src/main/java/com/example/SkillsetProfiling/Controller/QuestionOptionsController.java
@@ -1,75 +1,75 @@
-package com.example.SkillsetProfiling.Controller;
-
-
-import com.example.SkillsetProfiling.Dto.Question_Options_DTO;
-import com.example.SkillsetProfiling.Exception.QuestionOptionsNotFoundException;
-import com.example.SkillsetProfiling.Service.Implementation.Question_Bank_Service;
-import com.example.SkillsetProfiling.Service.Implementation.Question_Options_Service;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@AllArgsConstructor
-@CrossOrigin("*")
-@RestController
-@RequestMapping("api/question_options")
-public class QuestionOptionsController {
-    private Question_Options_Service service;
-
-
-    @PostMapping("/add")
-    public ResponseEntity<Question_Options_DTO> addQuestionOptions(@RequestBody Question_Options_DTO questionOptionsDTO) {
-        Question_Options_DTO addedQuestionOptions = service.addQuestionOption(questionOptionsDTO);
-        return new ResponseEntity<>(addedQuestionOptions, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/getFromId/{questionID}")
-    public ResponseEntity<Question_Options_DTO> getQuestionOptionsByQuestionID(@PathVariable Integer questionID) {
-        try {
-            Question_Options_DTO questionOptionsDTO = service.getQuestionOptionByQuestionID(questionID);
-            return new ResponseEntity<>(questionOptionsDTO, HttpStatus.OK);
-        } catch (QuestionOptionsNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/getAll")
-    public ResponseEntity<List<Question_Options_DTO>> getAllQuestionOptions() {
-        List<Question_Options_DTO> questionOptionsList = service.getAllQuestionOptions();
-
-        if (questionOptionsList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-
-        return new ResponseEntity<>(questionOptionsList, HttpStatus.OK);
-    }
-
-    @PutMapping("/update/{questionID}")
-    public ResponseEntity<Question_Options_DTO> updateQuestionOptions(@PathVariable Integer questionID,
-                                                                @RequestBody Question_Options_DTO updatedQuestionOptionsDTO) {
-        try {
-            Question_Options_DTO updatedQuestionOptions = service.updateQuestionOption(questionID, updatedQuestionOptionsDTO);
-            return new ResponseEntity<>(updatedQuestionOptions, HttpStatus.OK);
-        } catch (QuestionOptionsNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/delete/{questionID}")
-    public ResponseEntity<Void> deleteQuestionOptions(@PathVariable Integer questionID) {
-        try {
-            boolean deleted = service.deleteQuestionOption(questionID);
-            if (deleted) {
-                return new ResponseEntity<>(HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } catch (QuestionOptionsNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-}
+//package com.example.SkillsetProfiling.Controller;
+//
+//
+//import com.example.SkillsetProfiling.Dto.Question_Options_DTO;
+//import com.example.SkillsetProfiling.Exception.QuestionOptionsNotFoundException;
+//import com.example.SkillsetProfiling.Service.Implementation.Question_Bank_Service;
+//import com.example.SkillsetProfiling.Service.Implementation.Question_Options_Service;
+//import lombok.AllArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@AllArgsConstructor
+//@CrossOrigin("*")
+//@RestController
+//@RequestMapping("api/question_options")
+//public class QuestionOptionsController {
+//    private Question_Options_Service service;
+//
+//
+//    @PostMapping("/add")
+//    public ResponseEntity<Question_Options_DTO> addQuestionOptions(@RequestBody Question_Options_DTO questionOptionsDTO) {
+//        Question_Options_DTO addedQuestionOptions = service.addQuestionOption(questionOptionsDTO);
+//        return new ResponseEntity<>(addedQuestionOptions, HttpStatus.CREATED);
+//    }
+//
+//    @GetMapping("/getFromId/{questionID}")
+//    public ResponseEntity<Question_Options_DTO> getQuestionOptionsByQuestionID(@PathVariable Integer questionID) {
+//        try {
+//            Question_Options_DTO questionOptionsDTO = service.getQuestionOptionByQuestionID(questionID);
+//            return new ResponseEntity<>(questionOptionsDTO, HttpStatus.OK);
+//        } catch (QuestionOptionsNotFoundException e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//    @GetMapping("/getAll")
+//    public ResponseEntity<List<Question_Options_DTO>> getAllQuestionOptions() {
+//        List<Question_Options_DTO> questionOptionsList = service.getAllQuestionOptions();
+//
+//        if (questionOptionsList.isEmpty()) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        }
+//
+//        return new ResponseEntity<>(questionOptionsList, HttpStatus.OK);
+//    }
+//
+//    @PutMapping("/update/{questionID}")
+//    public ResponseEntity<Question_Options_DTO> updateQuestionOptions(@PathVariable Integer questionID,
+//                                                                @RequestBody Question_Options_DTO updatedQuestionOptionsDTO) {
+//        try {
+//            Question_Options_DTO updatedQuestionOptions = service.updateQuestionOption(questionID, updatedQuestionOptionsDTO);
+//            return new ResponseEntity<>(updatedQuestionOptions, HttpStatus.OK);
+//        } catch (QuestionOptionsNotFoundException e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//    @DeleteMapping("/delete/{questionID}")
+//    public ResponseEntity<Void> deleteQuestionOptions(@PathVariable Integer questionID) {
+//        try {
+//            boolean deleted = service.deleteQuestionOption(questionID);
+//            if (deleted) {
+//                return new ResponseEntity<>(HttpStatus.OK);
+//            } else {
+//                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//            }
+//        } catch (QuestionOptionsNotFoundException e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//}
--- src/main/java/com/example/SkillsetProfiling/Dto/Question_Options_DTO.java
+++ src/main/java/com/example/SkillsetProfiling/Dto/Question_Options_DTO.java
@@ -1,21 +1,21 @@
-package com.example.SkillsetProfiling.Dto;
-import com.example.SkillsetProfiling.Entity.Question_Bank;
-import com.example.SkillsetProfiling.Entity.Question_Difficulty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.sql.Timestamp;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Question_Options_DTO {
-    Question_Bank questionBank;
-    private String optionA;
-    private String optionB;
-    private String optionC;
-    private String optionD;
-}
+//package com.example.SkillsetProfiling.Dto;
+//import com.example.SkillsetProfiling.Entity.Question_Bank;
+//import com.example.SkillsetProfiling.Entity.Question_Difficulty;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.sql.Timestamp;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Question_Options_DTO {
+//    Question_Bank questionBank;
+//    private String optionA;
+//    private String optionB;
+//    private String optionC;
+//    private String optionD;
+//}
